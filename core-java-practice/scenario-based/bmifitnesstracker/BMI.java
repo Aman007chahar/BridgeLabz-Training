52c26ca71bdca5a4f7647e9b4d6c56a0ba42192c
AmanChahar <aman007chahar@gmail.com>
2026-01-03 14:29:41 +0530

    solved problem 2nd max

--- core-java-practice/scenario-based/bmifitnesstracker/BMI.java
+++
@@ -1,5 +0,0 @@
-package bmifitnesstracker;
-
-public class BMI {
-    //
-}
--- core-java-practice/scenario-based/bmifitnesstracker/BMICalculator.java
+++
@@ -1,8 +0,0 @@
-package bmifitnesstracker;
-
-public class BMICalculator {
-    public int bmiCalculator(int weight, int height){
-        int bmi = weight / (height*height);
-        return bmi;
-    }
-}
