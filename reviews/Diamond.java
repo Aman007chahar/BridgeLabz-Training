57cf53e3f1fcb1737501af3db0c2184a716b01ab
AmanChahar <aman007chahar@gmail.com>
2026-01-03 12:57:13 +0530

    solved problem 2nd max

--- reviews/Diamond.java
+++ reviews/Diamond.java
@@ -1,22 +1,24 @@
 public class Diamond {
     public static void main(String[] args) {
 
-        //2nd max from array no for loop no sort
-
-        int[] arr = new int[]{5,1,6,9,2,8,1,10};
+        int[] arr = {5, 1, 6, 9, 67, 2, 8, 1, 10, 34};
 
         int max = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
 
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] > max){
+        for (int i = 0; i < arr.length; i++) {
+
+
+
+            if (arr[i] > max) {
                 secondMax = max;
                 max = arr[i];
-
-                //secondMax = max;
+            }
+            else if (arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
             }
         }
-        System.out.println(secondMax);
 
+        System.out.println(secondMax);
     }
 }
